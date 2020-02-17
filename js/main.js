214f8c1689848098e02106b38704072ad9c5c36c
FankaPanik <sdscD23VDvd1Dwsv>
2020-02-17 08:27:20 +0500

    slider

--- js/main.js
+++ js/main.js
@@ -1,153 +1,40 @@
-function Slider(sliderId) {
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    let id = document.getElementById(sliderId);
-	if(id) {
-		this.sliderRoot = id
-	}
-	else {
-		this.sliderRoot = document.querySelector('.slider')
-	};
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+let plusSlide = () => (showSlides(slideIndex += 1));
 
-	// Carousel objects
-	this.sliderList = this.sliderRoot.querySelector('.slider-list');
-	this.sliderElements = this.sliderList.querySelectorAll('.slider-element');
-	this.sliderElemFirst = this.sliderList.querySelector('.slider-element');
-	this.leftArrow = this.sliderRoot.querySelector('div.slider-arrow-left');
-	this.rightArrow = this.sliderRoot.querySelector('div.slider-arrow-right');
-	this.indicatorDots = this.sliderRoot.querySelector('div.slider-dots');
-    this.sliderLink = this.sliderList.querySelector('.pr__info-tab');
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+let minusSlide = () => (showSlides(slideIndex -= 1));  
 
-	// Initialization
-	this.options = Slider.defaults;
-	Slider.initialize(this)
-};
-
-Slider.defaults = {
-
-	// Default options for the carousel
-	loop: true,     // Бесконечное зацикливание слайдера
-	auto: false,     // Автоматическое пролистывание
-	arrows: true,   // Пролистывание стрелками
-	dots: true      // Индикаторные точки
-};
-
-Slider.prototype.elemPrev = function(num) {
-	num = num || 1;
-
-	let prevElement = this.currentElement;
-	this.currentElement -= num;
-	if (this.currentElement < 0) this.currentElement = this.elemCount-1;
-
-	if (!this.options.loop) {
-		if (this.currentElement == 0) {
-			this.leftArrow.style.display = 'none'
-		};
-		this.rightArrow.style.display = 'block'
-	};
-	
-	this.sliderElements[this.currentElement].style.opacity = '1';
-	this.sliderElements[prevElement].style.opacity = '0';
-
-	if (this.options.dots) {
-		this.dotOn(prevElement);
-        this.dotOff(this.currentElement)
-	}
-};
-
-Slider.prototype.elemNext = function(num) {
-	num = num || 1;
-	
-	let prevElement = this.currentElement;
-	this.currentElement += num;
-	if (this.currentElement >= this.elemCount) this.currentElement = 0;
-
-	if (!this.options.loop) {
-		if (this.currentElement == this.elemCount-1) {
-			this.rightArrow.style.display = 'none'
-		};
-		this.leftArrow.style.display = 'block'
-	};
-
-	this.sliderElements[this.currentElement].style.opacity = '1';
-	this.sliderElements[prevElement].style.opacity = '0';
-
-	if(this.options.dots) {
-		this.dotOn(prevElement);
-        this.dotOff(this.currentElement)
-	}
-};
-
-Slider.prototype.dotOn = function(num) {
-	this.indicatorDotsAll[num].style.cssText = 'background-color:#BBB; cursor:pointer;'
-};
-
-Slider.prototype.dotOff = function(num) {
-	this.indicatorDotsAll[num].style.cssText = 'background-color:#556; cursor:default;'
-};
-
-Slider.initialize = function(that) {
-
-	// Constants
-	that.elemCount = that.sliderElements.length; // Количество элементов
-
-	// Variables
-	that.currentElement = 0;
-	let bgTime = getTime();
-
-	// Functions
-	function getTime() {
-		return new Date().getTime();
-	};
-
-	if(that.elemCount >= 1) {   // показать первый элемент
-		that.sliderElemFirst.style.opacity = '1';
-	};
+/* Устанавливает текущий слайд */
+let currentSlide = (n) => (showSlides(slideIndex = n));
 
 
-	if(that.options.arrows) {  // инициализация стрелок
-		that.leftArrow.addEventListener('click', function() {
-			let fnTime = getTime();
-			if (fnTime - bgTime > 1000) {
-				bgTime = fnTime; that.elemPrev()
-			}
-		}, false);
-		that.rightArrow.addEventListener('click', function() {
-			let fnTime = getTime();
-			if(fnTime - bgTime > 1000) {
-				bgTime = fnTime; that.elemNext()
-			}
-		}, false)
-	}
-	else {
-		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
-	};
-
-	if(that.options.dots) {  // инициализация индикаторных точек
-		let sum = '', diffNum;
-		for(let i=0; i<that.elemCount; i++) {
-			sum += '<div class="dot"></div>'
-		};
-		that.indicatorDots.innerHTML = sum;
-		that.indicatorDotsAll = that.sliderRoot.querySelectorAll('div.dot', 'a.pr__tab');
-		// Назначаем точкам обработчик события 'click'
-		for(let n = 0; n < that.elemCount; n++) {
-			that.indicatorDotsAll[n].addEventListener('click', function() {
-				diffNum = Math.abs(n - that.currentElement);
-				if(n < that.currentElement) {
-					bgTime = getTime(); that.elemPrev(diffNum)
-				}
-				else if(n > that.currentElement) {
-					bgTime = getTime(); that.elemNext(diffNum)
-				}
-				// Если n == that.currentElement ничего не делаем
-			}, false)
-		};
-		that.dotOff(0);  // точка[0] выключена, остальные включены
-		for(let i = 1; i < that.elemCount; i++) {
-			that.dotOn(i)
-		}
-	}
-};
-
-new Slider();
-
+/* Основная функция слайдера */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("projects__item");
+    let dots = document.getElementsByClassName("slider-dots_item");
+    let title = document.getElementsByClassName("title pr__tab");
+    
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    for (i = 0; i < title.length; i++) {
+        title[i].className = title[i].className.replace(" active-title", "");
+    }
+    
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+    title[slideIndex - 1].className += " active-title";
+}
